bd813e152e0f583a12d822dac33ec8b8fb04b7f9
DiegoPenalba <dieh.penalba@gmail.com>
2025-08-06 14:19:54 -0300

    first commit

---
+++ EstructurasDeDatos/estructurasDeDatos.py
@@ -0,0 +1,37 @@
+# ----------------------------
+# TIPOS DE DATOS SECUENCIA / COLECCION
+# ----------------------------
+
+# Listas [Coleccion ordenada y mutable]
+lista_prueba = [1, 2, 3, 4, 5]
+
+# Tuplas (Coleccion ordenada e inmutable)
+tupla_prueba = (1, 2, 3, 4, 5)
+
+# rangos (Coleccion ordenada e inmutable de numeros)
+rango_prueba = range(1, 6)  # Equivalente a [1, 2, 3, 4, 5]
+
+# ----------------------------
+# TIPOS DE DATOS MAPEO (Mapping types)
+# ----------------------------
+
+# Diccionarios {Coleccion desordenada, mutable y de pares clave-valor}
+diccionario_prueba = {
+    "nombre": "Juan",
+    "edad": 30,
+    "ciudad": "Madrid"
+}
+
+# ----------------------------
+# TIPOS DE DATOS CONUJUNTOS
+# ----------------------------
+
+# sets {Coleccion desordenada, mutable y sin elementos duplicados}
+set_prueba = {1, 2, 3, 4, 5}
+
+# ----------------------------
+# TIPOS DE DATOS NULO
+# ----------------------------
+
+# NoneType (Representa la ausencia de valor)
+valor_nulo = None
